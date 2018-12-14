48ab4e5f283c6920e09ee3bfe19b1f76074c6a6d
xbsj <xbsjiang@163.com>
2018-12-15 00:18:07 +0800

    处理器映射器

---
+++ springmvcmapper/src/main/java/com/xbsj/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.xbsj.controller;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class HelloController extends AbstractController {
+
+    @Override
+    protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println("HelloController.handleRequestInternal");
+        return new ModelAndView("index");
+    }
+
+}
